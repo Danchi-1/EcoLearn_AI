eaac18d915a4dd009818c5bdd074cfcdf11e4fda
Ohachor Daniel <ohachordaniel2021@gmail.com>
2025-07-04 13:07:56 +0100

    backend optimization added

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -52,7 +52,7 @@ document.getElementById('loginForm')?.addEventListener('submit', async (e) => {
     const email = document.getElementById('login-email').value;
     const password = document.getElementById('login-password').value;
 
-    const res = await fetch('/login', {
+    const res = await fetch('/https://ecolearn-backend.up.railway.app/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
